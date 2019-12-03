844542b50f2e0f33b0a910779fcb030b3a882cee
Alex Martinez <alexandr3martin3z@gmail.com>
2019-12-03 23:30:23 +0000

    Built out Menu and Film Details methods in CLI class

--- lib/studio_ghibli_cli/cli.rb
+++ lib/studio_ghibli_cli/cli.rb
@@ -18,24 +18,63 @@ class StudioGhibliCli::CLI
     
    
   def list 
-    puts "\nHere's a list of all Studio Ghibli films."
+    puts "\nHere's a list of all Studio Ghibli films.\n"
     sleep(0.5)
    
      StudioGhibliCli::Film.all.each.with_index(1) do |film, i|
-        #binding.pry
       puts "#{i}. #{film.title}"
       sleep(0.2)
     end
   end
   
+  def film_details(input)
+    
+    film = StudioGhibliCli::Film.all[input.to_i - 1]
+        
+        sleep(0.25)
+        puts "\n--------------------------------------\n"
+        sleep(1)
+        puts "\nTitle: #{film.title}\n"
+        sleep(1)
+        puts "\nDescription: #{film.description}\n"
+        sleep(1)
+        puts "\nDirector: #{film.director}\n"
+        sleep(1)
+        puts "\nRelease Date: #{film.release_date}\n"
+        sleep(1)
+        puts "\nRT Score: #{film.rt_score}\n"
+        puts "\n--------------------------------------\n"
+  end
+  
   def menu
+    input = nil
     
-    
+    while input != "exit"
+      sleep(1)
+      puts "\nInput a number from the list of films to view it's details.\n"
+      sleep(1)
+      puts "\nType 'list' to view the full list again.\n"
+      sleep(1)
+      puts "\nOr, if you'd like to exit, type 'exit'.\n"
+      input = gets.strip.downcase
+      
+      
+      if input.to_i > 0 
+        film_details(input)
+      elsif input == "list"
+        list
+      elsif input == "exit"
+        goodbye
+      else 
+        puts "\nINVALID INPUT: type 'list' or 'exit'"
+      end 
+    end
   end 
   
   
   def goodbye
-    puts "Thanks for checking out these Studio Ghibli films!"
+    puts "thanks for checking out these studio ghibli films!".upcase
+    sleep(2)
   end
     
      
