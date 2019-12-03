f3b43d5bcc2bb944bdda4c4b439c4c90a56634fc
Alex Martinez <alexandr3martin3z@gmail.com>
2019-12-03 07:24:42 +0000

    Made changes

--- lib/studio_ghibli_cli/cli.rb
+++ lib/studio_ghibli_cli/cli.rb
@@ -6,13 +6,22 @@ class StudioGhibliCli::CLI
   def start
     StudioGhibliCli::API.new.fetch
     StudioGhibliCli::Film.all.each do |film|
-      puts film.title 
+      puts film.title
+      sleep(0.2)
     end 
+    
     menu
+    
   end 
   
-  def menu
+  def list 
     
   end
+  
+  
+  def goodbye
+    puts "Thanks for checking out these Studio Ghibli films!"
+  end
+    
      
 end
