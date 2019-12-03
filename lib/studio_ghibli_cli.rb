39b7ac5e46f03e2d72477dd92cb8366f1ca065dd
Alex Martinez <alexandr3martin3z@gmail.com>
2019-12-03 02:22:55 +0000

    first commit

---
+++ lib/studio_ghibli_cli.rb
@@ -0,0 +1,6 @@
+require "studio_ghibli_cli/version"
+
+module StudioGhibliCli
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/studio_ghibli_cli/version.rb
@@ -0,0 +1,3 @@
+module StudioGhibliCli
+  VERSION = "0.1.0"
+end
