345b37610fc7fe11865f856e6dcda3f0d45712e8
Alex Martinez <alexandr3martin3z@gmail.com>
2019-12-05 02:29:41 +0000

    Made user input for more information more abstract incase any future Studio Ghibli films are made

--- lib/studio_ghibli_cli.rb
+++ lib/studio_ghibli_cli.rb
@@ -1,5 +1,6 @@
 require 'pry'
 require 'httparty'
+require 'colorize'
 
 require_relative "./studio_ghibli_cli/version"
 require_relative "./studio_ghibli_cli/cli"
--- lib/studio_ghibli_cli/cli.rb
+++ lib/studio_ghibli_cli/cli.rb
@@ -12,7 +12,7 @@ class StudioGhibliCli::CLI
   end 
     
   def greeting
-      puts "\nWelcome anime fans!"
+      puts "\nWelcome anime fans!\n".upcase
       sleep(0.5)
   end
     
@@ -51,22 +51,22 @@ class StudioGhibliCli::CLI
     
     while input != "exit"
       sleep(1)
-      puts "\nInput a number from the list of films to view it's details.\n"
+      puts "\nInput a number from the list of films to view it's details.\n".yellow
       sleep(1)
-      puts "\nType 'list' to view the full list again.\n"
+      puts "\nType 'list' to view the full list again.\n".white
       sleep(1)
-      puts "\nOr, if you'd like to exit, type 'exit'.\n"
+      puts "\nOr, if you'd like to exit, type 'exit'.\n".red
       input = gets.strip.downcase
       
       
-      if input.to_i > 0 
+      if input.to_i > 0 && input.to_i < StudioGhibliCli::Film.all.size
         film_details(input)
       elsif input == "list"
         list
       elsif input == "exit"
         goodbye
       else 
-        puts "\nINVALID INPUT: type 'list' or 'exit'"
+        puts "\nINVALID INPUT: type 'list' or 'exit'".red
       end 
     end
   end 
